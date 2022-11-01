c7e94eda7c2341e86165bb354a104776c6c802a8
Aashish rana <aashishrana71135@gmail>
2022-11-01 14:14:39 -0700

    Java fundamentals

--- ReadChar.java
+++ ReadChar.java
@@ -3,7 +3,7 @@ public class ReadChar {
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the character");
-        char ch = sc.next().charAt(0);
+        char ch = sc.next().charAt(4);
         System.out.println("Character is " + ch); 
 
     }
